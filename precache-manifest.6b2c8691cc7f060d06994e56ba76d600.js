8ae2be60faa13202fe030e7cf52a2050e4d8a456
Ashtroe <ashton.thompson97@gmail.com>
2020-11-20 14:38:44 -0600

    Updates

---
+++ precache-manifest.6b2c8691cc7f060d06994e56ba76d600.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ccab1b92559536457068f911220c1e0",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "d47b42e7188171984473",
+    "url": "/weather/static/css/main.611d6b4e.chunk.css"
+  },
+  {
+    "revision": "3058b1c5fb2995d00726",
+    "url": "/weather/static/js/2.3480dc99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.3480dc99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d47b42e7188171984473",
+    "url": "/weather/static/js/main.25aba09d.chunk.js"
+  },
+  {
+    "revision": "65a135568c88d392bf25",
+    "url": "/weather/static/js/runtime-main.d8725fa2.js"
+  },
+  {
+    "revision": "8ff26c69dc53d41dcfbcc718f92e5439",
+    "url": "/weather/static/media/uv.8ff26c69.png"
+  }
+]);
